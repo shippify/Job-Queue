f97389a26a2e41ff71a95ec616ec5564ab3b01ba
markoscalderon <mcmarkos86@gmail.com>
2017-07-14 12:24:04 -0500

    Added kue and redis

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:6.9
+
+RUN apt-get update
+
+RUN apt-get install --no-install-recommends -y redis-server
+
+RUN rm -rf /var/lib/apt/lists/*
+
+RUN mkdir /app
+
+WORKDIR /app
+
+ADD package.json /app/package.json
+
+ADD index.js /app/index.js
+
+RUN npm install
+
+EXPOSE 3000 6379
+
+CMD service redis-server start & node index.js
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const kue = require('kue');
+const ui = require('kue-ui');
+const basicAuth = require('basic-auth-connect');
+
+const port = 3000;
+
+const redisConfig = {
+    host: process.env.JOB_QUEUE_REDIS_HOST || 'localhost',
+    port: process.env.JOB_QUEUE_REDIS_PORT || '6379'
+};
+
+if (process.env.JOB_QUEUE_REDIS_PASS) {
+    redisConfig.auth = process.env.JOB_QUEUE_REDIS_PASS;
+}
+
+const queue = kue.createQueue({
+    redis: redisConfig
+});
+
+queue.on('error', function(err) {
+    console.log('ERROR:');
+    console.log(err);
+});
+
+ui.setup({
+    apiURL: '/api',
+    baseURL: '/kue',
+    updateInterval: 5000
+});
+
+const app = express();
+
+if (process.env.JOB_QUEUE_USERNAME && process.env.JOB_QUEUE_PASSWORD) {
+  app.use(basicAuth(process.env.JOB_QUEUE_USERNAME, process.env.JOB_QUEUE_PASSWORD));
+}
+
+app.use('/api', kue.app)
+app.use('/kue', ui.app)
+app.listen(port);
